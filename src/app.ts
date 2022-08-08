45e45cb6082ba39ecf0a1836ec49e443bce24a1a
Kreee <contact@ohmykreee.top>
2022-08-08 15:54:26 +0800

    Chore: add comments and tsdoc

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { msgHandler } from "./handler"
 import { startIRC } from "./irc"
 import { exit } from "process"
 
+// 用于定义用于存储传入消息的变量
 interface msg {
   raw_text :string,
   text? :string,
@@ -12,7 +13,7 @@ interface msg {
   user_id :number
 }
 
-
+// 用于定义用于存储回复消息的变量
 interface msg_response {
   message_type :string,
   text :string,
@@ -20,14 +21,16 @@ interface msg_response {
   group_id? :number
 }
 
+// 用于定义用于传给 go-cqhttp 的变量
 interface msg_params {
-  message_type? :string,
+  message_type :string,
   user_id :number,
   group_id? :number,
   message :string,
   auto_escape? :boolean
 }
 
+// 初始化日志记录库
 const SimpleNodeLogger = require('simple-node-logger'),
 	opts = {
 		// logFilePath:'mylogfile.log',
@@ -35,6 +38,7 @@ const SimpleNodeLogger = require('simple-node-logger'),
 	},
 log = SimpleNodeLogger.createSimpleLogger( opts )
 
+// 初始化 WebSocket，并传入事件触发函数
 const client = new WebSocket(config.url + '?access_token=' + config.token)
 client.addEventListener('message', function (event) {
   ifNeedResponed(JSON.parse(event.data as string))
@@ -46,18 +50,31 @@ client.addEventListener('close' ,function(event) {
   log.error(`connection closed: ${event.reason}`)
   exit(1)
 })
+// 启动 irc.ts 中的 slate-irc 初始化
 startIRC()
 
+/**
+ * 当接收到 go-cqhttp 任意内容时触发
+ *
+ * @remarks
+ * 由 WebSocket 的 message 事件触发
+ *
+ * @param data - 接收消息的 object，内容可参考{@link https://github.com/botuniverse/onebot-11/blob/master/event/message.md}
+ * 
+ */
 function ifNeedResponed(data :any) :void {
+  // 判断是否为消息
   if (data.raw_message) {
     const msg :msg = {
       raw_text: data.raw_message as string,
       message_type: data.message_type as string,
       user_id: data.sender.user_id as number
     }
+    // 判断前缀是否为 kreee/Kreee，如是则除去前6个字符
     if (/^[Kk]reee/g.test(msg.raw_text)) {
       msg.text = msg.raw_text.slice(6)
       log.info(`[${msg.user_id}] ${msg.text}`)
+      // 判断是否为群消息，如是则传入群号至对应变量，判断完成传入处理后的内容至 fetchResponse()
       if(msg.message_type === 'group') {
         msg.group_id = data.group_id
         fetchResponse(msg)
@@ -66,66 +83,101 @@ function ifNeedResponed(data :any) :void {
       }
     }
   } else {
+    // 若判断为非消息，则传入 handleCallback 进行下一步处理
     handleCallback(data)
   }
 }
 
+// 获取回复，主要通过 handler.ts
+/**
+ * 获取回复内容
+ *
+ * @remarks
+ * 将来自 {@link ifNeedResponed()} 接收的消息字符串传递给 {@link msgHandler()} ,经过简单处理后并构造回复所需要的 object，传递给 {@link makeResponse()}
+ *
+ * @param msg - 接收消息的 object
+ * 
+ */
 function fetchResponse(msg: msg) :void {
+  // 执行来自 handler.ts 的 msgHandler()，并通过回调函数获取文字结果
   msgHandler(msg.text as string, function callback(reply) {
     const res :msg_response = {
       message_type: msg.message_type,
       text: reply,
       user_id: msg.user_id,
     }
+    // 判断是否返回没有命中规则的默认回复，是则写入日志警告
     if(res.text === '智商有点低，听不懂捏') {
       log.warn(`mismatch text:${msg.raw_text}`)
     }
+    // 判断是否为群消息，如是则在消息结尾加上at，并传入群号
     if (res.message_type === 'group') {
       res.group_id = msg.group_id,
       res.text = res.text + `\n[CQ:at,qq=${res.user_id}]`
     }
+    // 判断是否在开发模式，如是则在消息结尾加上 (Dev mode)
     if (config.debug) {
       res.text = res.text + `\n(Dev mode)`
     }
+    // 传入消息至 makeResponse()，回复消息
     makeResponse(res)
   })
 }
 
+/**
+ * 回复消息
+ *
+ * @remarks
+ * 将来自 {@link fetchResponse()} 的内容封装后通过 WebSocket 传回给 go-cqhttp，回复消息
+ *
+ * @param res - 回复消息的 object
+ * 
+ */
 function makeResponse(res :msg_response) :void {
   const msg_params :msg_params = {
     message: res.text,
     user_id: res.user_id,
+    message_type: res.message_type
   }
-  switch(res.message_type) {
-    case 'group':
-      msg_params.message_type = 'group'
-      msg_params.group_id = res.group_id
-      break
-    case 'private':
-      msg_params.message_type = 'private'
-      break
-    default:
-       log.error(`makeResponse: message_type is out of range: ${res.message_type}`)
-       exit(1)
+  // 判断是否为群消息，如是则传入群号
+  if (res.message_type === 'group') {
+    msg_params.group_id = res.group_id
+  // 如果 message_type 不是 group 或是 private，则输出错误并终止程序
+  } else if (res.message_type !== 'group' && res.message_type !== 'private') {
+  log.error(`makeResponse: message_type is out of range: ${res.message_type}`)
+  exit(1)
   }
-  
+  // 构建发送给 go-cqhttp 的消息主体
   const msg_send :object = {
     "action": "send_msg" as string,
     "params": msg_params as msg_params,
     "echo": `Response to ${msg_params.user_id}`
   }
-
+  //通过 WebSocket 发送消息给 go-cqhttp
   client.send(JSON.stringify(msg_send))
 }
 
+/**
+ * 处理非消息类型的内容
+ *
+ * @remarks
+ * 接收来自 {@link ifNeedResponed()} 的非接收消息的内容，并处理输出至日志中
+ *
+ * @param data - 非接收消息的 object，内容可参考{@link https://github.com/botuniverse/onebot-11/blob/master/event/meta.md}
+ * 
+ */
 function handleCallback(data :any) :void {
+  // 判断是否为消息发送后的反馈
   if (data.echo) {
     log.info(`callback: ${data.status}, retcode:${data.retcode}, echo:${data.echo}`)
+  // 判断是否为生命周期事件
   } else if (data.post_type == 'meta_event' && data.meta_event_type == 'lifecycle') {
     log.info(`${data.sub_type} user_id=${data.self_id}`)
+  // 判断是否为心跳事件
   } else if (data.post_type == 'meta_event' && data.meta_event_type == 'heartbeat') {
     log.info(`heartbeat interval=${data.interval}`)
+  // 无法判断时直接输出至日志
   } else {
-    log.warn(`unhandled: ${JSON.stringify(data)}`);
+    log.warn(`unhandled: ${JSON.stringify(data)}`)
   }
 }
--- src/handler.ts
+++ src/handler.ts
@@ -8,6 +8,7 @@ import { version } from '../package.json';
 import { getOSUStats } from "./irc"
 import { updateOSUStats } from "./irc"
 
+// 初始化日志记录库
 const SimpleNodeLogger = require('simple-node-logger'),
 	opts = {
 		// logFilePath:'mylogfile.log',
@@ -15,7 +16,18 @@ const SimpleNodeLogger = require('simple-node-logger'),
 	},
 log = SimpleNodeLogger.createSimpleLogger( opts )
 
+/**
+ * 处理消息并返回回复消息字符串
+ *
+ * @remarks
+ * 接受接收的消息的字符串，当该消息命中某种规则时，返回回复消息字符串
+ *
+ * @param msg - 接收消息的字符串
+ * @returns 回调函数，返回值为字符串
+ * 
+ */
 export function msgHandler(msg: string, callback: (reply :string) => void) :void {
+  // 定义用于存储回复消息字符的变量，并赋予“没有命中规则的默认回复”值
   let reply :string = '智商有点低，听不懂捏'
   if (msg === 'help'|| msg === '帮助' || msg === 'h') {
     reply = `支持：\n帮助/help/h\n关于\n在线\n色色\n星期四/星期几\n二次元\n动物/爆个照\n舔狗\n吃什么\nsh(上海证券交易所)/sz(深圳证券交易所)+股票代码\n查询列表\n更新在线列表\n谁最可爱`
@@ -30,10 +42,13 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
     callback(reply)
 
   } else if (msg === '就要色色！') {
+    // http get 获取 JSON 内容
     axios.get('https://api.waifu.im/random?is_nsfw=true')
       .then(res => {
+        // 通过 JSON 中的图片链接下载图片并存储在变量中
         axios.get(res.data.images[0].url, { responseType: 'stream' })
           .then(res => {
+            // 将下载的图片上传到 Gokapi 服务并返回链接
             axios.post(config.gokapiurl, {
               'file': res.data,
               'allowedDownloads': 0,
@@ -42,6 +57,7 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
               headers: {'apikey': config.gokapitoken, 'Content-Type': 'multipart/form-data'},
             })
               .then(res => {
+                // 构造回复消息，注意要避开风控关键词（链接、二维码等）
                 reply = `${res.data.FileInfo.HotlinkId}\n有效期3d`
                 callback(reply)
               })
@@ -59,6 +75,7 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
 
   } else if (msg === '查询列表') {
     let replytext :string = ''
+    // 遍历输出 list/osu.ts 中的内容
     for (const user of osuname) {
       replytext = `${replytext}${user.text}\n`
     }
@@ -66,16 +83,19 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
     callback(reply)
 
   } else if (msg === '更新在线列表') {
+    // 运行 irc.ts 中的 updateOSUStats() 以更新在线列表
     updateOSUStats(function(replytext) {
       reply = replytext
       callback(reply)
     })
 
   } else if (/谁最可爱/g.test(msg)) {
+    // 随机选取 list/cutie.ts 中的一项并回复
     reply = cutie[Math.floor(Math.random() * cutie.length)].text
     callback(reply)
 
   } else if (/在线/g.test(msg)) {
+    // 运行 irc.ts 中的 getOSUStats() 获取回复消息内容
     getOSUStats(function(replytext) {
       reply = replytext
       callback(reply)
@@ -87,8 +107,10 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
     callback(reply)
 
   } else if (/二次元/g.test(msg)) {
+    // 获取包含图片信息的 JSON 内容
     axios.get('https://api.yimian.xyz/img?type=moe&R18=false')
       .then(res => {
+        // 直接传递图片 url 给 go-cqhttp
         reply = `[CQ:image,file=${res.request.protocol}//${res.request.host}${res.request.path}]`
         callback(reply)
       })
@@ -98,6 +120,7 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
 
   } else if (/星期四/g.test(msg) || /星期几/g.test(msg)) {
     let today :number = new Date().getDay()
+    // 判断今天是否为星期四，如是则随机选取 list/kfc-vw50.ts 中的一项回复
     if (today === 4) {
       reply = vw50[Math.floor(Math.random() * vw50.length)].text
       callback(reply)
@@ -107,6 +130,7 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
     } 
     
   } else if (/舔狗/g.test(msg)) {
+    // 获取纯文字内容并回复
     axios.get('https://api.ixiaowai.cn/tgrj/index.php')
       .then(res => {
         reply = res.data as string
@@ -117,8 +141,10 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
       })
 
   } else if ((/动物/g.test(msg) || /爆个照/g.test(msg))) {
+    // 获取包含图片信息的 JSON 内容
     axios.get('https://api.tinyfox.dev/img?animal=yote&json')
       .then(res => {
+        // 直接传递图片 url 给 go-cqhttp
         reply = `[CQ:image,file=${res.request.protocol}//${res.request.host}${res.data.loc}]`
         callback(reply)
       })
@@ -127,16 +153,19 @@ export function msgHandler(msg: string, callback: (reply :string) => void) :void
       })
 
   } else if (/sh/g.test(msg) || /sz/g.test(msg)) {
+    // 直接传递图片 url 给 go-cqhttp，使用新浪 api，若股票信息不正确将会输出空图片
     reply = `[CQ:image,file=https://image.sinajs.cn/newchart/min/n/${msg}.gif]`
     callback(reply)
 
   } else if (/吃什么/g.test(msg)) {
+    // 随机选取 list/food.ts 中的一项回复
     reply = food[Math.floor(Math.random() * food.length)].text
     callback(reply)
 
   }
   
- else {
+  // 若未命中任何规则，则回复没有命中规则的默认回复
+  else {
     callback(reply)
   }
 

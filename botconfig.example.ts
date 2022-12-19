a1399f58b4821f419b3e37b7a1406385c75414ed
Kreee <kreeejiang@outlook.com>
2022-12-19 12:41:14 +0800

    Fix: re-add version output in /about

--- botconfig.example.ts
+++ botconfig.example.ts
@@ -1,21 +1,21 @@
 // 修改设置时，仅修改以下代码块！
 const config :botconf = {
   debug: false,
-  cqhttpUrl: "",
-  cqhttpToken: "",
+  cqhttpUrl: "ws://localhost:1234",
+  cqhttpToken: "this_is_a_very_long_token_for_cqhttp",
   adminQQ: [123456789, 987654321],
   description: "实用化，固定的命令与简单的触发关键字符。",
-  gokapiUrl: "",
-  gokapiToken: "",
-  osuIrcUsername: "",
-  osuIrcPassword: "",
+  gokapiUrl: "https://gokapi.example.site/api/",
+  gokapiToken: "this_is_a_very_long_token_for_gokapi",
+  osuIrcUsername: "Kreee",
+  osuIrcPassword: "this_is_a_very_long_passwd_for_irc",
   osuIrcIntervalMin: 5,
   osuIrcIntervalMax: 10,
-  nitterUrl: "https://nitter.net/",
+  nitterUrl: "https://nitter.example.site/",
   maxLogHistory: 100,
   osuClientId: 114514,
-  osuClientSecret: "",
-  ahrCWD: "./osuahr"
+  osuClientSecret: "this_is_a_very_long_secret_for_osu",
+  ahrCWD: "/usr/local/kbot-qq/osuahr"
 }
 
 // 以下内容请勿修改！

b732122e630fe2622f56292e4e59861e792667ee
Kreee <contact@ohmykreee.top>
2023-03-31 22:55:55 +0800

    Fix: potential query to other bot

--- src/handler.ts
+++ src/handler.ts
@@ -504,7 +504,7 @@ export function msgHandler(msg :Array<string>, qqid :number) :Promise<string | s
         const output: string = outputSlice.join("").replaceAll("[", "").replaceAll("]", "")
         // 判断是否为空
         if (output.length !== 0) {
-          resolve(output)
+          resolve(`“${output}”`)
         } else {
           resolve("[结果为空]")
         }
